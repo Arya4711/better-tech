11f833c405ce667aa99bce4389e2530bf43aa149
Sapender <willjpender@gmail.com>
2024-02-04 02:16:02 -0500

    Added Armour and Tools
    so much stuff

--- src/main/java/io/github/Sapender/RahoodTech/Initialisation/ItemInitialisation.java
+++ src/main/java/io/github/Sapender/RahoodTech/Initialisation/ItemInitialisation.java
@@ -2,8 +2,7 @@ package io.github.Sapender.RahoodTech.Initialisation;
 
 import io.github.Sapender.RahoodTech.RahoodTech;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -19,8 +18,27 @@ public class ItemInitialisation {
                             .nutrition(5)
                             .saturationMod(0.2f)
                             .build())));
+    //TOOLS UNDERNEATH//
+    public static final RegistryObject<PickaxeItem> TEST_PICKAXE = ITEMS.register("test_pickaxe",
+            () -> new PickaxeItem(TierInitialisation.TEST,7,2.5f,new Item.Properties().durability(10000))
+    );
+
+    //ARMOUR UNDERNEATH//
+    public static final RegistryObject<ArmorItem> TEST_HELMET = ITEMS.register("test_helmet",
+            () -> new ArmorItem(ArmourMaterialInitialisation.TEST,ArmorItem.Type.HELMET,new Item.Properties()));
+
+    public static final RegistryObject<ArmorItem> TEST_CHESTPLATE = ITEMS.register("test_chestplate",
+            () -> new ArmorItem(ArmourMaterialInitialisation.TEST,ArmorItem.Type.CHESTPLATE,new Item.Properties()));
+
+    public static final RegistryObject<ArmorItem> TEST_LEGGINGS = ITEMS.register("test_leggings",
+            () -> new ArmorItem(ArmourMaterialInitialisation.TEST,ArmorItem.Type.LEGGINGS,new Item.Properties()));
+
+    public static final RegistryObject<ArmorItem> TEST_BOOTS = ITEMS.register("test_boots",
+            () -> new ArmorItem(ArmourMaterialInitialisation.TEST,ArmorItem.Type.BOOTS,new Item.Properties()));
+
 
     //BLOCK ITEMS UNDERNEATH//
     public static final RegistryObject<BlockItem> TEST_BLOCK_ITEM = ITEMS.register("test_block",
             () -> new BlockItem(BlockInitialisation.TEST_BLOCK.get(), new Item.Properties()));
+
 }
---
+++ src/main/java/io/github/Sapender/RahoodTech/Initialisation/TagInitialisation.java
@@ -0,0 +1,14 @@
+package io.github.Sapender.RahoodTech.Initialisation;
+
+import io.github.Sapender.RahoodTech.RahoodTech;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class TagInitialisation {
+    public static final TagKey<Block> NEEDS_TEST_TOOL = tag("needs_test_tool");
+
+    private static TagKey<Block> tag(String name) { return BlockTags.create(new ResourceLocation(RahoodTech.MODID,name));
+    }
+}
